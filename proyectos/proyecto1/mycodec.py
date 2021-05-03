605cd709e4c96a05d706dc5589cdec0cb35891a9
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-05-03 06:18:27 -0400

    Añadido parte del informe al código

--- proyectos/proyecto1/mycodec.py
+++ proyectos/proyecto1/mycodec.py
@@ -3,6 +3,8 @@ import cv2 as cv
 import heapq
 from scipy import fftpack as fft
 from fnc import *
+import pickle
+import ast
 
 #Función que elimina el ruido de la imagen
 def denoise(frame):
@@ -17,22 +19,67 @@ def code(frame_filtrado):
     
     frame_ct = cuantizacion(frame_tr,80)
 
-    frame_destranformado = inv_transform(frame_ct)
-    
-    return frame_destranformado
+    # frame_ct se convierte en un vector
+    frame_ct = frame_ct.ravel()    
 
+    # Se lee el dendograma 
+    a_file = open("data.pkl", "rb")
+    new_dendo = pickle.load(a_file)
+    a_file.close()
 
-def decode(message):
+    # Se codifica el frame
+    texto_codificado = ""
+    for num in frame_ct:
+        texto_codificado += new_dendo[num]
+
+    # Se transforma a byte
+    b = bytearray()
+    for i in range(0, len(texto_codificado), 8):
+        byte = texto_codificado[i:i+8]
+        b.append(int(byte, 2))
+
+    #return frame_destranformado
+
+    return b
+
+def decode(b):
+
+    # Se carga el dendograma y se crea el inverso
+    a_file = open("data.pkl", "rb")
+    new_dendo = pickle.load(a_file)
+    a_file.close()
+    dendograma_inverso =  {codigo: simbolo for simbolo, codigo in dendograma.items()}
+
+    # Se toma el texto codificado en bytes y se transforma a una cadena de 1 y 0
+    texto_decodificado = [valor for k in range(len(b)) for valor in texto_codificado[k]]
+    texto_decodificado = ''.join(texto_codificado)
+
+    # Se decodifica con el dendograma inverso
+    codigo = ""
+    texto = ""
+    for bit in texto_decodificado:
+        codigo += bit
+        if codigo in dendograma_inverso:
+            texto += dendograma_inverso[codigo].astype(str)
+            texto += " "
+            codigo = ""
+
+    mensaje_final = np.array([float(x) for x in texto.split()], dtype=np.float64)
+    frame1 = mensaje_final.reshape(480,848)
+
+    # Se agrega la última esperanza para aprobar
+    frame_destranformado = inv_transform(frame1)
+
     #
     # Reemplaza la linea 24...
     #
     #frame_destranformado = inv_transform(frame_ct)
-    frame = np.frombuffer(bytes(memoryview(message)), dtype='uint8').reshape(480, 848)
+    #frame = np.frombuffer(bytes(memoryview(message)), dtype='uint8').reshape(480, 848)
     
     #
     # ...con tu implementación del bloque receptor: decodificador + transformación inversa
     #
-    return frame
+    return frame_destranformado
 
 
 
