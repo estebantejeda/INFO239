4531b20846266f5a2abb0ca9890b67330f1ccc90
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-07-19 04:47:50 -0400

    Finalizado trabajo

--- proyecto2/cliente.py
+++ proyecto2/cliente.py
@@ -1,16 +1,68 @@
-import socket
+# Basado en tdf2.2
 
-msgFromClient       ="Using Link Client 1"
-bytesToSend         = str.encode(msgFromClient)
+import socket
+import random
+import time
+
+msgFromClient       ="Esteban"
 serverAddressPort   = ("127.0.0.1", 20001)
 bufferSize          = 1024
-
-# Create a UDP socket at client side
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-# Send to server using created UDP socket
-print("Intentando enviar")
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-msg = "Message from Server {}".format(msgFromServer[0])
-print(msg)
+def init(msg):
+    count = len(msg)
+    k = 0
+    while(k < count):
+        x = send0(msg[k])
+        send1(x)
+        k+=1
+        x = send2(msg[k])
+        send3(x)
+        k+=1
+    return
+
+def send0(data):
+    sndpkt = make_pkt(0, data)
+    print("Enviando...")
+    udt_send(sndpkt)
+    return sndpkt
+
+def send1(sndpkt):
+    rcvpkt = UDPClientSocket.recvfrom(bufferSize)
+    rcvpkt = "{}".format(rcvpkt[0])
+    if(isACK(rcvpkt, 1)): 
+        udt_send(sndpkt)
+        send1(sndpkt)
+    return
+
+def send2(data):
+    sndpkt = make_pkt(1, data)
+    print("Enviando...")
+    udt_send(sndpkt)
+    return sndpkt
+
+def send3(sndpkt):
+    rcvpkt = UDPClientSocket.recvfrom(bufferSize)
+    rcvpkt = "{}".format(rcvpkt[0])
+    if(isACK(rcvpkt, 0)): 
+        udt_send(sndpkt)
+        send3(sndpkt)
+    return
+
+# Función que crea el paquete
+def make_pkt(num, data):
+    data = str(num)+"-"+data
+    return str.encode(data)
+
+# Función que envía el paquete al servidor
+def udt_send(sndpkt):
+    UDPClientSocket.sendto(sndpkt, serverAddressPort)
+    return
+
+def isACK(rcvpkt, num):
+    rcvpkt = rcvpkt[2]
+    num = str(num)
+    if(rcvpkt == num): return True
+    return False
+
+init(msgFromClient)
--- proyecto2/server.py
+++ proyecto2/server.py
@@ -4,27 +4,60 @@ import time
 localIP     = "127.0.0.1"
 localPort   = 20001
 bufferSize  = 1024
-msgFromServer       = "Datagram Acepted"
-bytesToSend         = str.encode(msgFromServer)
-
-# Create a datagram socket
+msgFromServer       = "SERVER"
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-# Bind to address and ip
 UDPServerSocket.bind((localIP, localPort))
-print("Link Available")
 
-# Listen for incoming datagrams
-while(True):
+def recive0():
+    print("Servidor Disponible...")
     
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-    message = bytesAddressPair[0]
-    address = bytesAddressPair[1]
-    #clientMsg = "Message from Client:{}".format(message)
-    print("Link bussy")
-    clientMsg = format(message) 
-    print(clientMsg)
-    time.sleep(30)
-    # Sending a reply to client
+    rcvpkt = UDPServerSocket.recvfrom(bufferSize)
+    message = rcvpkt[0]
+    address = rcvpkt[1]
+    message = format(message)
+
+    if(has_seq1(message)): 
+        udt_send("1", address)
+        recive0()
+
+    time.sleep(3)
+    print(message[4])
+
+    return
+
+def recive1():
+    print("Servidor Disponible...")
+    
+    rcvpkt = UDPServerSocket.recvfrom(bufferSize)
+    message = rcvpkt[0]
+    address = rcvpkt[1]
+    message = format(message)
+
+    if(has_seq0(message)):
+        udt_send("0", address)
+        recive1()
+
+    time.sleep(3)
+    print(message[4])
+
+    return
+
+def has_seq1(rcvpkt):
+    rcvpkt = rcvpkt[2]
+    if(str(rcvpkt) == "1"): return True
+    return False
+
+def has_seq0(rcvpkt):
+    rcvpkt = rcvpkt[2]
+    if(str(rcvpkt) == "0"): return True
+    return False
+
+def udt_send(num, address):
+    bytesToSend = str.encode(num)
     UDPServerSocket.sendto(bytesToSend, address)
-    print("Link Available")
+    return
+
+
+while(True):
+    recive0()
+    recive1()
